a5110a1e770affd79e638564f20f872cb63489d1
Ethan Lee <leesnap@umich.edu>
2020-05-28 18:51:56 -0400

    game board and pieces

---
+++ pieces.js
@@ -0,0 +1,242 @@
+const ROW = 21;
+const COL = 10;
+const BS = 20;  //block size
+const ctx = document.getElementById("game").getContext("2d");
+
+//empty board
+let board = [];
+for(let i = 0; i < ROW; ++i){
+    board[i] = [];
+    for(let j = 0; j < COL; ++j){
+        //empty spaces
+        board[i][j] = "black";
+    }
+}
+
+const I = [
+	[
+		[0, 0, 0, 0],
+		[1, 1, 1, 1],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+	],
+	[
+		[0, 0, 1, 0],
+		[0, 0, 1, 0],
+		[0, 0, 1, 0],
+		[0, 0, 1, 0],
+	],
+	[
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[1, 1, 1, 1],
+		[0, 0, 0, 0],
+	],
+	[
+		[0, 1, 0, 0],
+		[0, 1, 0, 0],
+		[0, 1, 0, 0],
+		[0, 1, 0, 0],
+	]
+];
+
+const J = [
+	[
+		[1, 0, 0],
+		[1, 1, 1],
+		[0, 0, 0]
+	],
+	[
+		[0, 1, 1],
+		[0, 1, 0],
+		[0, 1, 0]
+	],
+	[
+		[0, 0, 0],
+		[1, 1, 1],
+		[0, 0, 1]
+	],
+	[
+		[0, 1, 0],
+		[0, 1, 0],
+		[1, 1, 0]
+	]
+];
+
+const L = [
+	[
+		[0, 0, 1],
+		[1, 1, 1],
+		[0, 0, 0]
+	],
+	[
+		[0, 1, 0],
+		[0, 1, 0],
+		[0, 1, 1]
+	],
+	[
+		[0, 0, 0],
+		[1, 1, 1],
+		[1, 0, 0]
+	],
+	[
+		[1, 1, 0],
+		[0, 1, 0],
+		[0, 1, 0]
+	]
+];
+
+const O = [
+	[
+		[0, 0, 0, 0],
+		[0, 1, 1, 0],
+		[0, 1, 1, 0],
+		[0, 0, 0, 0],
+	]
+];
+
+const S = [
+	[
+		[0, 1, 1],
+		[1, 1, 0],
+		[0, 0, 0]
+	],
+	[
+		[0, 1, 0],
+		[0, 1, 1],
+		[0, 0, 1]
+	],
+	[
+		[0, 0, 0],
+		[0, 1, 1],
+		[1, 1, 0]
+	],
+	[
+		[1, 0, 0],
+		[1, 1, 0],
+		[0, 1, 0]
+	]
+];
+
+const T = [
+	[
+		[0, 1, 0],
+		[1, 1, 1],
+		[0, 0, 0]
+	],
+	[
+		[0, 1, 0],
+		[0, 1, 1],
+		[0, 1, 0]
+	],
+	[
+		[0, 0, 0],
+		[1, 1, 1],
+		[0, 1, 0]
+	],
+	[
+		[0, 1, 0],
+		[1, 1, 0],
+		[0, 1, 0]
+	]
+];
+
+const Z = [
+	[
+		[1, 1, 0],
+		[0, 1, 1],
+		[0, 0, 0]
+	],
+	[
+		[0, 0, 1],
+		[0, 1, 1],
+		[0, 1, 0]
+	],
+	[
+		[0, 0, 0],
+		[1, 1, 0],
+		[0, 1, 1]
+	],
+	[
+		[0, 1, 0],
+		[1, 1, 0],
+		[1, 0, 0]
+	]
+];
+
+const PIECES = [
+    [Z,"red"],
+    [S,"green"],
+    [T,"purple"],
+    [O,"yellow"],
+    [L,"orange"],
+    [I,"cyan"],
+    [J,"blue"]
+];
+
+let nextP = [];
+let curP;
+
+function checkCollision(x, y, pattern){
+    if(x < 0 || x >= COL || y >= ROW){
+        return true;
+    }
+    for(let r = 0; r < pattern.length; ++r){
+        for(let c = 0; c < pattern[r].length; ++c){
+            if(pattern[r][c]){
+                if(board[this.y + r][this.x + c] != "black"){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+class Piece{
+    constructor(squares, color){
+        this.squares = squares;
+        this.color = color;
+        this.pattern = squares[0];
+        this.num = 0;
+        this.x = 3;
+        this.y = 0;
+    }
+    drawPiece(){
+        for(let r = 0; r < this.pattern.length; ++r){
+            for(let c = 0; c < this.pattern[r].length; ++c){
+                if(this.pattern[r][c]){
+                    drawSquare(this.y + r, this.x + c, this.color);
+                }
+            }
+        }
+    }
+    erasePiece(){
+        for(let r = 0; r < 3; ++r){
+            for(let c = 0; c < 3; ++c){
+                if(this.pattern[r][c]){
+                    drawSquare(this.y + r, this.x + c, "black");
+                }
+            }
+        }
+    }
+    //once piece can't go down anymore
+    stop(){
+        //TODO
+    }
+    moveLeft(){
+        if(!checkCollision(this.x-1, this.y, this.pattern)){
+            this.x = this.x - 1;
+        }
+    }
+    moveRight(){
+        if(!checkCollision(this.x+1, this.y, this.pattern)){
+            this.x = this.x + 1;
+        }
+    }
+    moveDown(){
+        if(!checkCollision(this.x, this.y+1, this.pattern)){
+            this.y = this.y + 1;
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,62 @@
+function drawSquare(r, c, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(c*BS, r*BS, BS, BS);
+
+    ctx.strokeStyle = "gray";
+    ctx.strokeRect(c*BS, r*BS, BS, BS);
+}
+
+function drawBoard(){
+    for(let i = 1; i < ROW; ++i){
+        for(let j = 0; j < COL; ++j){
+            this.drawSquare(i, j, board[i][j]);
+        }
+    }
+
+    //outline
+    ctx.strokeStyle = "gray";
+    ctx.strokeRect(0, 20, 200, 400);
+}
+
+drawBoard();
+
+//unbiased shuffle algorithm. Fisher-Yates Shuffle.
+function shuffle(batch){
+    var curIndex = batch.length;
+
+    while (0 !== curIndex) {
+        var rand = Math.floor(Math.random() * curIndex);
+        curIndex -= 1;
+
+        var temp = batch[curIndex];
+        batch[curIndex] = batch[rand];
+        batch[rand] = temp;
+    }
+
+    return batch;
+}
+
+function genNewBatch(){
+    let temp = [0, 1, 2, 3, 4, 5, 6];
+    shuffle(temp);
+    for(let i = 0; i < 7; ++i){
+        nextP.push(PIECES[temp[i]]);
+    }
+}
+
+genNewBatch();
+
+curP = new Piece(nextP[0][0], nextP[0][1]);
+nextP.shift();
+
+function getNewPiece(){
+    let toDel = curP;
+    curP = new Piece(nextP[0][0], nextP[0][1]);
+    nextP.shift();
+    delete toDel;
+    if(nextP.length() < 5){
+        genNewBatch();
+    }
+}
+
+curP.drawPiece();
